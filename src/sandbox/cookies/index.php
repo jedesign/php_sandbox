68f864509f566af49f0df58f378f935099c7fba9
Marco Rieser <marco.rieser@jedesign.ch>
2014-07-04 23:49:47 +0200

    Add an example for cookies

---
+++ src/sandbox/cookies/index.php
@@ -0,0 +1,33 @@
+<?php
+// if no expire date is set,
+// the session time is used
+//setcookie('fontSize', 25);
+
+// setcookie('fontSize', 25, time() + 60 * 30, '/');
+
+// Need to access as an array?
+setcookie('prefs[fontSize]', 25);
+setcookie('prefs[favoriteCategory]', 'news');
+setcookie('prefs[screenWidth]', '1024');
+?>
+<html>
+<head>
+    <title></title>
+    <style>
+        body {
+            font-size: <?= htmlspecialchars($_COOKIE['fontSize']); ?>
+        }
+    </style>
+</head>
+<body>
+
+<?php
+if ( isset($_COOKIE['prefs']) ) {
+    foreach($_COOKIE['prefs'] as $key => $val ) {
+        echo '<li>' . htmlspecialchars($key) . ': ' . htmlspecialchars($val);
+    }
+}
+?>
+
+</body>
+</html>
